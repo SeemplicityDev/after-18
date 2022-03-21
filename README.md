e91990e371e719f2143d936c470f2b01bcd90d72
avi-city <avi@seemplicity.io>
2022-03-21 02:29:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# After-18
+
+![Are you sure?](https://github.com/avi-city/after-18/blob/main/images/are-you-sure.png?raw=true)
+
+
+## Brief
+A Chrome Extension that works only on Github’s “pull request” pages, and changes the text on the “Merge” button after a given hour (18:00 in our case) to remind the developer about the consequences of pressing that button.
+
+You can read our blog post [here](https://todo)
+
+## How to install
+* Clone the repository
+```
+git clone https://github.com/avi-city/after-18.git
+```
+
+* Open Chrome
+* Go to the Chrome Extensions page (`chrome://extensions/`)
+* Click on `Load unpacked`
+* Choose the `after-18` directory
---
+++ content-script.js
@@ -0,0 +1,33 @@
+const hours = 18;
+
+function addRemoveWarning(text) {
+    const prefix = "Are you sure you want to "
+    const suffix = " after " + hours + ":00?";
+    var now = new Date()
+    if (now.getHours() > hours) {
+        if (text.startsWith(prefix)) {
+            return text;
+        } else {
+            return prefix + text + suffix;
+        }
+    } else {
+        if (text.startsWith(prefix)) {
+            text.substr(prefix.length, text.length - prefix.length - suffix.length)
+        } else {
+            return text;
+        }
+    }
+}
+
+var interval = setInterval(function () {
+    var buttons = document.getElementsByClassName("merge-box-button");
+    for (var i=0; i < buttons.length; i++) {
+        var oldText = buttons[i].innerHTML;
+        var newText = addRemoveWarning(oldText);
+        if (oldText != newText) {
+            console.log(oldText);
+            console.log(newText);
+            buttons[i].innerHTML = newText;
+        }
+    }
+}, 100);
